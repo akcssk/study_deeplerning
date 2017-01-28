9fe202b2ab6d0a96368ffa04d97784d72bd0a665
sakuzone <s-akashi@glad-cube.com>
2017-01-29 00:27:46 +0900

    modify perceptron

--- logic_gate.py
+++ logic_gate.py
@@ -1,16 +1,27 @@
-
-def perceptron (x1, x2, w1, w2, theta):
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+import numpy as np
+def perceptron (x1, x2, w1, w2, bias):
+#2017/01/29
+#パーセプトロンはXORを出せない。直線を引いて分断するだけなので
+#非線形の分類でないと無理
+#ここで多層パーセプトロンですよ
+    x = np.array([x1, x2])
+    w = np.array([w1, w2])
+    tmp = np.sum(w*x) + bias
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    else:
         return 1
 def AND (x1, x2):
-    return perceptron(x1, x2, w1=0.5, w2=0.5, theta=0.7)
+    return perceptron(x1, x2, w1=0.5, w2=0.5, bias=-0.7)
 def NAND (x1, x2):
-    return perceptron(x1, x2, w1=-0.5, w2=-0.5, theta=-0.7)
+    return perceptron(x1, x2, w1=-0.5, w2=-0.5, bias=0.7)
 def OR (x1, x2):
-    return perceptron(x1, x2, w1=0.5, w2=0.5, theta=0.3)
+    return perceptron(x1, x2, w1=0.5, w2=0.5, bias=-0.3)
+def XOR (x1, x2):
+    #思いついたので作るただ、引き算は反則だ
+    #return OR(x1, x2) - AND(x1, x2)
+    #回路的に正しいのはこっち、
+    return AND(NAND(x1, x2), OR(x1, x2))
 
 print('-------AND--------')
 print(AND(0,0))
@@ -27,3 +38,8 @@ print(OR(0,0))
 print(OR(0,1))
 print(OR(1,0))
 print(OR(1,1))
+print('-------XOR--------')
+print(XOR(0,0))
+print(XOR(0,1))
+print(XOR(1,0))
+print(XOR(1,1))
